f3b5005252e8d1b84849494be85f60e0c57c3d43
NattawatC <90686843+NattawatC@users.noreply.github.com>
2023-03-30 15:53:01 +0700

    Create application.py

---
+++ application.py
@@ -0,0 +1,53 @@
+from PySide6.QtWidgets import QStackedWidget
+
+from models import *
+from views import *
+from controllers import *
+
+
+class Application(QStackedWidget):
+
+    def __init__(self):
+        QStackedWidget.__init__(self, None)
+        self.setFixedSize(1280, 720)
+        self.setWindowTitle("Naar Raan")
+
+        self.current_user = None
+
+        # Login Page
+        self.login_page = LoginView(self, LoginView(), LoginModel())
+
+        # home Page
+        self.recipe_page = None
+
+        # start page
+        self.addWidget(self.login_page.view)
+
+    def initialize_page(self) -> None:
+        "set up method for user."
+        if self.current_user is None:
+            return
+
+        self.recipe_page = RecipeView(
+            self, RecipeView(), RecipeModel(), self.current_user)
+
+        # self.order_page = OrderPage(OrderView(), OrderModel())
+
+        # replace widget if already exist
+        self.insertWidget(1, self.recipe_page.view)
+        self.move_to_home()
+
+    def set_current_user(self, user: User) -> None:
+        self.current_user = user
+
+    def move_to_login(self):
+        self.current_user = None
+        self.login_page.clear_input_field()
+        self.setCurrentIndex(0)
+
+    def move_to_home(self):
+        self.setCurrentIndex(1)
+
+    def start(self) -> None:
+        "driver method."
+        self.showFullScreen()
